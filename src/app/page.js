d7d6fca17f79197f89f0480b13b3d75834179b1c
José A. Muñoz Jiménez <jamj2000@gmail.com>
2024-11-10 18:31:58 +0100

    rename to nxform

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,38 @@
 import { Users } from '@/lib/users'
-import { userDelete, userUpdate, userCreate } from './actions'
+import { userDelete, userUpdate, userCreate } from '@/lib/actions'
+
+
 
 export default function Home() {
+ 
   console.log(Users)
+ 
   return (
     <main>
-      <form id='insertar' key={crypto.randomUUID().replaceAll("-", "")} style={{ 'padding': '30px', 'border': 'solid 1px gray', 'marginTop': '20px' }}>
+
+      <form action={userCreate} id='insertar'>
         <input type='hidden' name='id' defaultValue={crypto.randomUUID().replaceAll("-", "")}></input>
+       
         <label htmlFor='nombre'>Usuario</label>
         <input type='text' id='nombre' name='nombre'></input>
+       
         <label htmlFor='edad'>Edad</label>
         <input type='text' id='edad' name='edad'></input>
-        <button formAction={userCreate}>Crear</button>
+
+        <button>Crear</button>
       </form>
 
       {Users.map(user => (
 
-        <form key={user.id} style={{ 'padding': '30px', 'border': 'solid 1px gray', 'marginTop': '20px' }}>
+        <form key={user.id}>
           <input type='hidden' name='id' defaultValue={user.id}></input>
+         
           <label htmlFor='nombre'>Usuario</label>
           <input type='text' id='nombre' name='nombre' defaultValue={user.nombre}></input>
+         
           <label htmlFor='edad'>Edad</label>
           <input type='text' id='edad' name='edad' defaultValue={user.edad}></input>
+         
           <button formAction={userUpdate}>Actualizar</button>
           <button formAction={userDelete}>Eliminar</button>
         </form>
